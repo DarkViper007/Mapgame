f65f2cda9df33fb1f543f1a201e70e0b9f50e01d
devlooper <emerald.mihail@gmail.com>
2016-08-03 20:05:15 +0300

    color sheme

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 $(document).ready(function(e) {
-		$(document).find('#part1').append('<div class="player" id="player1"><img src="http://icons.veryicon.com/png/Avatar/Face%20Avatars/Male%20Face%20O3.png" alt=""></div>');
-		$(document).find('#part11').append('<div class="player" id="player2"><img src="http://findicons.com/files/icons/1072/face_avatars/300/fh02.png" alt=""></div>');
-		$(document).find('#part21').append('<div class="player" id="player3"><img src="http://findicons.com/files/icons/206/looney_tunes/128/gossamer_angry.png" alt=""></div>');
-		$(document).find('#part31').append('<div class="player" id="player4"><img src="http://findicons.com/files/icons/1072/face_avatars/300/fd03.png" alt=""></div>');
+		$(document).find('#part1').append('<div class="player" id="player1"><img src="http://icons.veryicon.com/png/Avatar/Face%20Avatars/Male%20Face%20O3.png" alt=""></div>').addClass('playcolor1');
+		$(document).find('#part11').append('<div class="player" id="player2"><img src="http://findicons.com/files/icons/1072/face_avatars/300/fh02.png" alt=""></div>').addClass('playcolor2');
+		$(document).find('#part21').append('<div class="player" id="player3"><img src="http://findicons.com/files/icons/206/looney_tunes/128/gossamer_angry.png" alt=""></div>').addClass('playcolor3');
+		$(document).find('#part31').append('<div class="player" id="player4"><img src="http://findicons.com/files/icons/1072/face_avatars/300/fd03.png" alt=""></div>').addClass('playcolor4');
 		
-		$('.cub').on('click', function(){
+	$('.cub').on('click', function(){
 		if(!$(this).hasClass('NotTurn')){
 			var number = 1 + Math.floor(Math.random() * 6);
-				$(this).animate({ top: '25px' },500).text(number);
-				$(this).animate({ top: '20px' },500);
+				$(this).animate({ top: '35px' },500).text(number);
+				$(this).animate({ top: '30px' },500);
 			var CurrentPlayer = $(this).attr('data-cub');
-
+			var CurrentPlayerConst = $(this).attr('data-cub');
 			var PlayerPlace = $(document).find('#player'+CurrentPlayer).parent().attr('data-place');
 			var ChangePlace = +PlayerPlace + +number;
 
 			var PlayerObject = $(document).find('#player'+CurrentPlayer);
 
 
-
+			$(document).find('#player'+CurrentPlayer).parent().removeClass('playcolor'+CurrentPlayer); 
 			$(this).addClass('NotTurn'); 
 
 
@@ -31,15 +31,14 @@ $(document).ready(function(e) {
 
 			if(ChangePlace <= 40){
 				var PositionTo = $(document).find('#part'+ChangePlace).position();
-		// 	PlayerObject.parent().animate({'position': 'fixed','top': PositionTo.top, 'left': PositionTo.left },3000, function() {
 
-  // });
 			$(document).find('#part'+ChangePlace).append(PlayerObject);
-
+			$(document).find('#part'+ChangePlace).addClass('playcolor'+CurrentPlayerConst);
 		}
 			else {
 				ChangePlace = ChangePlace - 40;
 					$(document).find('#part'+ChangePlace).append(PlayerObject);
+					$(document).find('#part'+ChangePlace).addClass('playcolor'+CurrentPlayerConst);
 			}
 
 		}
--- template/footer.tpl
+++ template/footer.tpl
@@ -1,5 +1,8 @@
 <!-- footer -->
 <footer class="footer footer_main" role="contentinfo">
+<div class="blacksnow">
+	<div class="event"></div>
+</div>
 <script src='<?php echo BASE_URL."/js/jquery-3.1.0.min.js"; ?>'></script>
 <script src='<?php echo BASE_URL."/js/script.js"; ?>'></script>
 	</body>
